963bc9452163344a4110126ea7399720d939547b
Mark Anthony Bagasol <markbagasol@gmail.com>
2019-06-02 05:55:49 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,6 +14,8 @@
 def speak_to_grandma
   if phrase != phrase.upcase()
     return "HUH?! SPEAK UP SONNY!"
-  else 
+  elsif 
     return "NO, NOT SINCE 1938!"
+  else phrase = "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
 end
